c69af106538f4b218371e07a6c1ac6ac8d8c4b2c
Pat Somaru <patso@likewhatevs.io>
2026-02-14 18:20:21 -0500

    propagate localversion
    propagate localversion to kernel build
    
    Signed-off-by: Pat Somaru <patso@likewhatevs.io>

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -20,6 +20,9 @@ echo "::endgroup::"
 
 cd "$KERNEL_DIR"
 
+SHA=$(git rev-parse --short=12 HEAD)
+export LOCALVERSION="__${VNG_KERNEL_NAME:-local}__${SHA}"
+
 BUILD_ARGS=(--build --force)
 if [ -n "${VNG_KCONFIG:-}" ]; then
     KCONFIG_PATH="$GITHUB_WORKSPACE/$VNG_KCONFIG"
